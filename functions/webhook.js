060b1a6c10f5903f4850b7e60ca0d538530ef654
chrisweber <cweber@firehydrant.io>
2023-11-20 12:40:59 -0500

    fixed recursive

--- functions/webhook.js
+++ functions/webhook.js
@@ -4,34 +4,45 @@ exports.handler = async function(event, context) {
     try {
         // Parse the incoming payload
         const payload = JSON.parse(event.body);
-        const incidentName = payload.data.incident.name + "-second channel";
+        let incidentName = payload.name;
+
+        // Append "-second channel" to the incident name if not already present
+        if (!incidentName.includes("-second channel")) {
+            incidentName += "-second channel";
+        }
 
         // Construct new incident data for Firehydrant
         const newIncidentData = {
             "name": incidentName,
-            // ...other fields populated as needed
+            "started_at": payload.started_at,
+            "impacts": payload.impacts,
+            // Include other fields as needed
+            // "summary": "Your summary here",
+            // "description": "Your description here",
+            // ...
         };
 
         // Send a POST request to Firehydrant
-        // const response = await axios.post('https://api.firehydrant.io/v1/incidents', newIncidentData, {
-        //     headers: {
-        //         'Authorization': 'Bearer fhb-08cfef6f54a0238ce39315fc562e7217',
-        //         'Content-Type': 'application/json'
-        //     }
-        // });
+        // Uncomment the following code to enable the POST request
 
-        // Handle the response
-        console.log(response.data);
+        const response = await axios.post('https://api.firehydrant.io/v1/incidents', newIncidentData, {
+            headers: {
+                'Authorization': 'Bearer fhb-08cfef6f54a0238ce39315fc562e7217', // Use your actual API token
+                 'Content-Type': 'application/json'
+             }
+         });
+         console.log(response.data);
 
+        // Return a successful response
         return {
             statusCode: 200,
-            body: JSON.stringify({ message: "Incident created successfully" })
+            body: JSON.stringify({ message: "Incident creation initiated successfully" })
         };
     } catch (error) {
         console.error("Error: ", error);
         return {
             statusCode: 500,
-            body: JSON.stringify({ message: "Error creating incident" })
+            body: JSON.stringify({ message: "Error initiating incident creation" })
         };
     }
 };
