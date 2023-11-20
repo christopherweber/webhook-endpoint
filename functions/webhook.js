38d61f48d17e58bcc01ecd62c7ae645e56011ee1
chrisweber <cweber@firehydrant.io>
2023-11-20 12:30:28 -0500

    changed code to accept incidents

--- functions/webhook.js
+++ functions/webhook.js
@@ -1,23 +1,37 @@
 const axios = require('axios');
 
-exports.handler = async (event) => {
-    // Only allow POST
-    if (event.httpMethod !== 'POST') {
-        return { statusCode: 405, body: 'Method Not Allowed' };
-    }
-
+exports.handler = async function(event, context) {
     try {
+        // Parse the incoming payload
         const payload = JSON.parse(event.body);
-        console.log(payload)
+        const incidentName = payload.data.incident.name + "-second channel";
 
-        // Manipulate the payload or make an API request
-        // Example: const response = await axios.post('API_URL', payload);
+        // Construct new incident data for Firehydrant
+        const newIncidentData = {
+            "name": incidentName,
+            // ...other fields populated as needed
+        };
+
+        // Send a POST request to Firehydrant
+        const response = await axios.post('https://api.firehydrant.io/v1/incidents', newIncidentData, {
+            headers: {
+                'Authorization': 'Bearer fhb-08cfef6f54a0238ce39315fc562e7217',
+                'Content-Type': 'application/json'
+            }
+        });
+
+        // Handle the response
+        console.log(response.data);
 
         return {
             statusCode: 200,
-            body: JSON.stringify({ message: 'Webhook received and processed' })
+            body: JSON.stringify({ message: "Incident created successfully" })
         };
     } catch (error) {
-        return { statusCode: 500, body: 'Internal Server Error' };
+        console.error("Error: ", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Error creating incident" })
+        };
     }
 };
